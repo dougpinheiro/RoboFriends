306dd0ab34752b19ec2b84fb86950a5b50872eeb
Douglas Pinheiro <dougcpinheiro@yahoo.com>
2018-06-16 11:01:36 -0300

    First commit

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+{
+id : 1,
+name : "Douglas Pinheiro",
+username : "dougpinheiro",
+email : "dougcpinheiro@yahoo.com"
+},
+{
+id : 2,
+name : "Bruna Gomes",
+username : "brunagomes",
+email : "brunagomes@yahoo.com"
+},
+{
+id : 3,
+name : "Maitê Pinheiro",
+username : "maitepinheiro",
+email : "maitepinheiro@yahoo.com"
+},
+{
+id : 4,
+name : "Cauê Pinheiro",
+username : "cauepinheiro",
+email : "cauepinheiro@yahoo.com"
+},
+{
+id : 5,
+name : "Berna Ribeiro",
+username : "bernaribeiro",
+email : "bernaribeiro@yahoo.com"
+}
+];
