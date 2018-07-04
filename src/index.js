f5c4386701cc2be4b0d04360d7058ecd66c76b0e
Douglas Pinheiro <dougcpinheiro@yahoo.com>
2018-07-03 22:16:42 -0300

    More about redux

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import { Provider} from 'react-redux';
-import { createStore } from 'redux'; 
+import { createStore , applyMiddleware, combineReducers } from 'redux'; 
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 ReactDOM.render(
-                    <Provider store={store}>
-                        <App />
-                    </Provider>, document.getElementById('root'));
-registerServiceWorker();
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+    registerServiceWorker();
